e67ff59010edaa6054100813eecd11e8288e653b
SreemanSajib <dsajib318@gmail.com>
2025-11-06 08:28:10 +0200

    add some new info

---
+++ git_cheet.py
@@ -0,0 +1,20 @@
+# clone: git clone <-some link->
+# folder -> code -> HTTPS -> copy link
+
+# status: git status
+
+# see all the files: ls
+# see hidden files: ls -a
+
+# modified/untracked file: add -> commit
+
+# Untracked: new files that git doesn't yet track
+# Modified: files that git is already tracking but have been changed
+# Staged: files is ready to be committed
+# unmodified: files that are unchanged since the last commit
+
+# add: add new or changed files in your working directory to the git staging area
+# git add <- file name->
+
+# commit: it is the record of changes
+# git commit -m "some message" 
